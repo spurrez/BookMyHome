e34fb9f4badf5ca75a7f55ecd240441b42cd6a0a
Christopher Vestergaard Jensen <cvje61684@edu.ucl.dk>
2025-02-21 14:32:49 +0100

    Add project files.

---
+++ BookMyHome.Core/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMyHome.Core
+{
+	internal class Booking
+	{
+        public int BookingId { get; set; }
+		public DateOnly CheckIn { get; set; }
+		public DateOnly CheckOut { get; set; }
+
+    }
+}
