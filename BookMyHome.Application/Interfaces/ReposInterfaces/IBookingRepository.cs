29b7b39a8b2b01264b2e638dbabdf752de7c582b
Christopher Vestergaard Jensen <cvje61684@edu.ucl.dk>
2025-02-24 00:23:43 +0100

    Service class for booking

---
+++ BookMyHome.Application/Interfaces/ReposInterfaces/IBookingRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookMyHome.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BookMyHome.Application.Interfaces.ReposInterfaces
+{
+    public interface IBookingRepository
+    {
+		Task<IEnumerable<Booking>?> GetAllBookings();
+		Task<Booking?> GetBookingById(Guid id);
+		Task<Booking> CreateBooking(Booking booking);
+		Task<Booking> UpdateBooking(Guid id, Booking booking);
+		Task<bool> DeleteBooking(Guid id);
+
+	}
+}
---
+++ BookMyHome.Application/Services/BookingService.cs
@@ -0,0 +1,55 @@
+﻿using BookMyHome.Application.Interfaces.ServicesInterfaces;
+using BookMyHome.Application.Interfaces.ReposInterfaces;
+using BookMyHome.Core.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMyHome.Application.Services
+{
+    public class BookingService : IBookingService
+	{
+		private readonly IBookingRepository _bookingRepository;
+
+		public BookingService(IBookingRepository bookingRepository)
+		{
+			_bookingRepository = bookingRepository;
+		}
+		
+		public async Task<IEnumerable<Booking>> GetAllBookings()
+		{
+			return await _bookingRepository.GetAllBookings();
+		}
+
+		public async Task<Booking?> GetBookingById(Guid id)
+		{
+			return await _bookingRepository.GetBookingById(id);
+		}
+
+		public async Task<Booking> CreateBooking(Booking booking)
+		{
+			var existingBookings = await _bookingRepository.GetAllBookings();
+			if (booking.IsOverLapping(existingBookings))
+				throw new Exception("The booking overlaps with another.");
+
+			return await _bookingRepository.CreateBooking(booking);
+		}
+
+		public async Task<Booking> UpdateBooking(Guid id, Booking booking)
+		{
+			var existingBookings = await _bookingRepository.GetAllBookings();
+			if (booking.IsOverLapping(existingBookings))
+				throw new Exception("The booking overlaps with another.");
+			return await _bookingRepository.UpdateBooking(id, booking);
+		}
+
+		public async Task<bool> DeleteBooking(Guid id)
+		{
+			return await _bookingRepository.DeleteBooking(id);
+		}
+	}
+}
